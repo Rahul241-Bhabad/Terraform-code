c26025ae102cb5308e0da8cc97ec66d5fac6a55a
Rahul241-Bhabad <rahulbhabad241@gmail.com>
2024-10-08 14:05:39 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,16 @@
+variable "ami_id" {
+    type = string
+    description = "AMI ID for mumbai region"
+}
+
+variable "instance_type" {
+    type = string
+}
+
+variable "icount" {
+    type = number
+}
+
+variable "key" {
+    type = string
+}
