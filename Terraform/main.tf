d07b9367933ef7a71900ba4060e503661d259e40
Rahul241-Bhabad <rahulbhabad241@gmail.com>
2024-10-10 16:07:15 +0530

    Create main.tf

---
+++ Terraform/main.tf
@@ -0,0 +1,16 @@
+module "ec2" {
+    source = "./Module/EC2"
+    ami_id = var.ami_id
+    instance_type = var.instance_type
+    key = var.key
+}
+
+module "vpc" {
+    source = "./Module/VPC"
+    vpc_cidr_block = var.vpc_cidr_block
+    aws_subnet = var.aws_subnet
+    az = var.az
+    public_ip = var.public_ip
+    port_numbers = var.port_numbers
+  
+}
